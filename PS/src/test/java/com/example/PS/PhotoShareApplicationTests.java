6c953310ae0ae9b9fa4f140cdc4b68db5e81895f
Ronan de Wee <deweeronan@gmail.com>
2021-11-22 08:05:20 +0200

    First commit!

---
+++ PS/src/test/java/com/example/PS/PhotoShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
